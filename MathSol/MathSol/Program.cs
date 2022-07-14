ffbbdb20eb89011b546f80396d57fa3932ebfffd
kordiP <nasikordov@gmail.com>
2022-07-14 20:08:01 +0300

    Updated the Program.cs to greet the user and get the type of problem they want solved.

--- MathSol/MathSol/Program.cs
+++ MathSol/MathSol/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MathSol
 {
@@ -10,11 +6,40 @@ namespace MathSol
     {
         static void Main()
         {
-            Console.WriteLine("What type of mathematical problem do you want solved? ");
+            int stage = 1;
+            Console.CursorVisible = false;
+            Console.WriteLine("Hello! This is MathSol by kordiP. I can solve many types of math problems and now I will try to solve yours. So, what is the type of your problem?");
+            Console.WriteLine("(Type H for help, C to clear text)");
+            Console.CursorVisible = true;
             string input = Console.ReadLine();
-        }
-        static void Help() // could be used at any time for help. / prints out all options
-        {
+            bool succ = false;
+            while (succ == false)
+            {
+                if (WorkCode.Checks(input, stage) != 0)
+                {
+                    Console.WriteLine(WorkCode.Checks(input, stage));
+                    succ = true;
+                }
+                else 
+                {
+                    switch (input)
+                    {
+                        case "H":
+                            WorkCode.Helper(1);
+                            break;
+                        case "C":
+                            Console.Clear();
+                            break;
+                        default:
+                            Console.WriteLine("Hmm... I couldn't find that.");
+                            WorkCode.Helper(stage);
+                            Console.WriteLine("Try with one of these!");
+                            break;
+                    }
+                    input = Console.ReadLine();
+                }   
+            }
+            stage++;
 
         }
     }
